a6f32894ed7e76cb57bbe22df89c09a15fd3528a
Euge <eugelito@outlook.com>
2021-10-28 18:49:04 +0100

    Coming soon page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import clubLogo from './images/acbelfast.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={clubLogo} className="App-logo" alt="logo" />
+        <p>Follow our instagram for latest news on the club launch</p>
+        <a href="https://www.instagram.com/acbelfastfc/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram fa-2x" aria-hidden="true"></i></a>
       </header>
     </div>
   );

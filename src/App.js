2af7b1cf7a9c35f7d0b636d7b0b1f68027b6b4f1
Euge <eugelito@outlook.com>
2021-10-29 17:37:12 +0100

    updated image sizing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={clubLogo} className="App-logo" alt="logo" />
-        <h1>Coming soon</h1>
-        <br/>
+        <div class="follow-us">
         <b>Follow us for the latest news on the club launch</b>
         <hr/>
+        </div>
         <div className="social-links">
         <a href="https://www.instagram.com/acbelfast/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram fa-2x" aria-hidden="true"></i></a>
         <a href="https://www.facebook.com/athleticclubbelfast" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>

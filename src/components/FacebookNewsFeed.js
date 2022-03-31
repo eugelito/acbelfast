c746c72a3a16304fbde57825d8af4edf4c59c7c9
Eugelito Troyo <eugelito@outlook.com>
2022-03-31 17:51:52 +0100

    Fix className to class for facebook chat plugin

--- src/components/FacebookNewsFeed.js
+++ src/components/FacebookNewsFeed.js
@@ -5,7 +5,7 @@ const FacebookNewsFeed = () => {
   return (
     <div className="facebook-newsfeed">
       <h2>Like us on <i className="fab fa-facebook fax" aria-hidden="true"></i> Facebook</h2>
-      <div className="fb-page" data-href="https://www.facebook.com/athleticclubbelfast/" data-tabs="timeline" data-width="364" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/athleticclubbelfast/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/athleticclubbelfast/">Athletic Club Belfast</a></blockquote></div>
+      <div className="fb-page" data-href="https://www.facebook.com/athleticclubbelfast/" data-tabs="timeline" data-width="364" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/athleticclubbelfast/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/athleticclubbelfast/">Athletic Club Belfast</a></blockquote></div>
     </div>
   );
 };

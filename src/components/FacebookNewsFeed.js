4fecf2297ccabf15dbba689efe69322d1cfcb0db
Eugelito Troyo <eugelito@outlook.com>
2022-05-15 20:41:20 +0100

    Facebook feed using react-facebook package

--- src/components/FacebookNewsFeed.js
+++ src/components/FacebookNewsFeed.js
@@ -1,12 +1,20 @@
 import "./FacebookNewsFeed.scss";
+import { FacebookProvider, Page } from 'react-facebook';
 
 const FacebookNewsFeed = () => {
 
   return (
-    <div className="facebook-newsfeed">
-      <h2>Like us on <i className="fab fa-facebook fax" aria-hidden="true"></i> Facebook</h2>
-      <div className="fb-page" data-href="https://www.facebook.com/acbelfast/" data-tabs="timeline" data-width="364" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/athleticclubbelfast/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/athleticclubbelfast/">Athletic Club Belfast</a></blockquote></div>
-    </div>
+    <>
+      <div className="facebook-newsfeed">
+        <h2>
+          Like us on <i className="fab fa-facebook fax" aria-hidden="true"></i>{" "}
+          Facebook
+        </h2>
+        <FacebookProvider appId="1234518782840956753076789">
+          <Page href="https://www.facebook.com/acbelfast" tabs="timeline" />
+        </FacebookProvider>
+      </div>
+    </>
   );
 };
 

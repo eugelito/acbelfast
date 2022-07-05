7292cb124b3dd868965bf8e0a02b8d8c08fa70ae
Eugelito Troyo <eugelito@outlook.com>
2022-07-05 09:35:02 +0100

    Updated training schedule and fetch league table data from api

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 //import News from "./components/News";
 import Result from "../Result";
 import Fixture from "../Fixture";
@@ -7,97 +7,101 @@ import FacebookNewsFeed from "../FacebookNewsFeed";
 import placeholderimage from "../../images/placeholderImage.png";
 import Header from "../Header";
 import "./Home.scss";
-import { useNavigate } from 'react-router-dom';
-
-
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const navigateToContact = () => navigate('/Contact');
+  const navigateToContact = () => navigate("/Contact");
 
-    return (
-      <>
-        <Header />
-        {/** HOMEPAGE */}
-        <div className="container__about">
-          <div className="container--centered">
-            <div className="about__founded">
-              <h2>Athletic Club Belfast</h2>
-              <span>Est. 2021</span>
-            </div>
-            <div>
-              <p className="about__description">
-                Senior Men's Amateur Football Club based in Belfast, Northern
-                Ireland. The club have a firsts and an over 35s team playing in
-                the Belfast and District Football League (Division 5) and Emerge Invitational League (Section D) respectively.
-              </p>{" "}<br/>
-              <p>The teams train on Mondays (7pm to 9pm) and Thursdays (8pm to 9pm) at Dunville Park, Falls Road, Belfast BT12 6AN.</p>
-              <br />
-              <button
-                className="getInTouchBtn"
-                type="button"
-                onClick={navigateToContact}
-              >
-                Get in touch
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="container__sponsors">
-          <div className="container--centered sponsors__centered">
-            <b className="container__divider">OUR SPONSORS</b>
-            <ul className="footer__sponsors">
-              <li>
-                <img
-                  src={placeholderimage}
-                  style={{ width: "50px", height: "50px" }}
-                  alt="placeholderImage"
-                />
-              </li>
-              <li>
-                <img
-                  src={placeholderimage}
-                  style={{ width: "50px", height: "50px" }}
-                  alt="placeholderImage"
-                />
-              </li>
-              <li>
-                <img
-                  src={placeholderimage}
-                  style={{ width: "50px", height: "50px" }}
-                  alt="placeholderImage"
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="container">
-          <div className="column__results__fixtures">
-            <Result />
-            <Fixture />
-          </div>
-          <div className="column__one">
-            {/** <News /> */}
-            <League />
-          </div>
-          <div className="column__two">
-            <FacebookNewsFeed />
+  return (
+    <>
+      <Header />
+      {/** HOMEPAGE */}
+      <div className="container__about">
+        <div className="container--centered">
+          <div className="about__founded">
+            <h2>Athletic Club Belfast</h2>
+            <span>Est. 2021</span>
           </div>
           <div>
-            <div className="row__social">
-              <h2 className="instafeed__heading">
-                Follow us on{" "}
-                <i className="fab fa-instagram fa" aria-hidden="true"></i>{" "}
-                Instagram <br/>@acbelfast
-              </h2>
-              <div id="instafeed-container"></div>
-            </div>
+            <p className="about__description">
+              Senior Men's Amateur Football Club based in Belfast, Northern
+              Ireland. The club have a firsts and an over 35s team playing in
+              the Belfast and District Football League (Division 5) and Emerge
+              Invitational League (Section D) respectively.
+            </p>{" "}
+            <br />
+            <p>
+              The teams train on Tuesdays and Thursdays (7pm to 9pm) at Hydebank
+              Playing Fields, 4 Newtownbreda Rd, Belfast BT8 6QB
+            </p>
+            <br />
+            <button
+              className="getInTouchBtn"
+              type="button"
+              onClick={navigateToContact}
+            >
+              Get in touch
+            </button>
           </div>
         </div>
-        {/** END OF HOMEPAGE */}
-      </>
-    );
-}
+      </div>
+      <div className="container__sponsors">
+        <div className="container--centered sponsors__centered">
+          <b className="container__divider">OUR SPONSORS</b>
+          <ul className="footer__sponsors">
+            <li>
+              <img
+                src={placeholderimage}
+                style={{ width: "50px", height: "50px" }}
+                alt="placeholderImage"
+              />
+            </li>
+            <li>
+              <img
+                src={placeholderimage}
+                style={{ width: "50px", height: "50px" }}
+                alt="placeholderImage"
+              />
+            </li>
+            <li>
+              <img
+                src={placeholderimage}
+                style={{ width: "50px", height: "50px" }}
+                alt="placeholderImage"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="container">
+        <div className="column__results__fixtures">
+          <Result />
+          <Fixture />
+        </div>
+        <div className="column__one">
+          {/** <News /> */}
+          <League />
+        </div>
+        <div className="column__two">
+          <FacebookNewsFeed />
+        </div>
+        <div>
+          <div className="row__social">
+            <h2 className="instafeed__heading">
+              Follow us on{" "}
+              <i className="fab fa-instagram fa" aria-hidden="true"></i>{" "}
+              Instagram <br />
+              @acbelfast
+            </h2>
+            <div id="instafeed-container"></div>
+          </div>
+        </div>
+      </div>
+      {/** END OF HOMEPAGE */}
+    </>
+  );
+};
 
-export default Home
+export default Home;

9aeffbf650203c0965f2fce44f10607990e7c12f
Eugelito Troyo <eugelito@outlook.com>
2022-12-28 19:06:40 +0000

    hide access token

--- public/instafeed-config.js
+++ public/instafeed-config.js
@@ -1,12 +1,11 @@
 // instafeed
-// import config from "../src/config";
-// const accessToken = config.INSTAGRAM_ACCESS_KEY;
+import config from "../src/config";
+const accessToken = config.INSTAGRAM_ACCESS_KEY;
 var userFeed = new Instafeed({
   get: "user",
   target: "instafeed-container",
   resolution: "low_resolution",
-  accessToken:
-    IGQVJYMldwaFdMekRVR29KVGZA0ZAk5XWUpqNHAzNUE2d3ctOGhKWXBoODh2Wks3aXEtbng4UFBQcE9TVUliSEE2X3otMkRnQ1RPQlV5cmFzem13eDVJOVR1bHRQWHZA6cnFZAS1ZAJLUJVVkZAqMWl5V3BKQQZDZD,
+  accessToken: accessToken,
   sortBy: "most-recent",
   limit: 6,
 });

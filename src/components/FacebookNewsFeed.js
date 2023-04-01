a7e3ee8740f487b435002e3bbc76911872980340
Eugelito Troyo <eugelito@outlook.com>
2023-04-01 19:48:08 +0100

    generate unique key

--- src/components/FacebookNewsFeed.js
+++ src/components/FacebookNewsFeed.js
@@ -2,14 +2,16 @@ import React from "react";
 import { FacebookProvider, Page } from "react-facebook";
 
 const FacebookNewsFeed = () => {
+  const [key, setKey] = useState(0);
+
   return (
     <>
-      <div className="facebook-newsfeed">
+      <div className="facebook-newsfeed" key={key}>
         <h2>
           Like us on <i className="fab fa-facebook fax" aria-hidden="true"></i>{" "}
           Facebook
         </h2>
-        <FacebookProvider appId="196175799810100" xfbml={true}>
+        <FacebookProvider appId="196175799810100">
           <Page href="https://www.facebook.com/acbelfast" tabs="timeline" />
         </FacebookProvider>
       </div>
